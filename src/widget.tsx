e647a7b9495561a42efd34b31e1d25c4634e2a5a
tushar12357 <110051562+tushar12357@users.noreply.github.com>
2025-08-04 19:32:10 +0530

    css

--- src/widget.tsx
+++ src/widget.tsx
@@ -16,7 +16,7 @@ class ReactWidget extends HTMLElement {
     const container = document.createElement("div");
     const link = document.createElement("link");
     link.rel = "stylesheet";
-    link.href = "https://multi-thunder-route-gvt9es0uk-tushar12357s-projects.vercel.app/style.css";
+    link.href = "https://multi-thunder-route.vercel.app/style.css";
 
     // Append the stylesheet and container to the Shadow DOM
     this.shadowRoot?.appendChild(link);
